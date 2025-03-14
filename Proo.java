7c5a48cbf5f755d573ed264a3693b491fe7d0b60
pecho-56 <prince861076@gmail.com>
2025-03-14 14:18:05 +0530

    added

---
+++ Proo.java
@@ -0,0 +1,13 @@
+public class Proo {
+    public static void main(String[] args) {
+        int i = 0;
+
+        for (i = 0; i < 10; i++) {
+            System.out.println(i);
+            if (i == 7) {
+                break; 
+            }
+        }
+    }
+}
+
---
+++ met.java
@@ -0,0 +1,11 @@
+public class met
+{
+    public static void main(String[] args)
+     {
+        String[] fruits={"apple","orange","Mango"};
+        System.out.println(fruits[0]);
+        System.out.println(fruits[1]);
+        System.out.println(fruits[2]);
+        System.out.println("Enter the fruits:"+fruits.length);
+    }
+}
